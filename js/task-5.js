d92140c2417b4fd9f5f8030340ddaa55e791263f
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-03-27 21:07:24 +0300

    task-5 fix

--- js/task-5.js
+++ js/task-5.js
@@ -4,25 +4,26 @@ let credits;
 switch (country.toLowerCase()) {
     case 'китай':
         credits = 100;
-        alert(`Доставка в ${country} буде коштувати ${credits} кредитів`);
         break;
     case 'чилі':
         credits = 250;
-        alert(`Доставка в ${country} буде коштувати ${credits} кредитів`);
         break;
     case 'австралія':
         credits = 170;
-        alert(`Доставка в ${country} буде коштувати ${credits} кредитів`);
         break;
     case 'індія':
         credits = 80;
-        alert(`Доставка в ${country} буде коштувати ${credits} кредитів`);
         break;
     case 'ямайка':
         credits = 120;
-        alert(`Доставка в ${country} буде коштувати ${credits} кредитів`);
         break;
     
     default:
-        alert('У вашій країні доставка недоступна');
+        credits = null;
+}
+
+if (credits === null) {
+    alert('У вашій країні доставка недоступна');
+} else {
+    alert(`Доставка в ${country} буде коштувати ${credits} кредитів`);
 }
---
+++ js/task-6.js
@@ -0,0 +1,9 @@
+let total = 0;
+let input = prompt('Введіть число')
+console.log(input);
+input = Number(input);
+console.log(typeof (input));
+
+total = total + input;
+console.log(total);
+console.log(typeof (total));
