d5c17e0c755f20e7338d0be5cdd7203ea7b3f428
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-03-27 20:24:40 +0300

    task-5 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,4 +1,28 @@
 const country = prompt('Вкажіть країну доставки: ')
-console.log(country.toLowerCase());
+let credits;
 
-///SWITCH
+switch (country.toLowerCase()) {
+    case 'китай':
+        credits = 100;
+        alert(`Доставка в ${country} буде коштувати ${credits} кредитів`);
+        break;
+    case 'чилі':
+        credits = 250;
+        alert(`Доставка в ${country} буде коштувати ${credits} кредитів`);
+        break;
+    case 'австралія':
+        credits = 170;
+        alert(`Доставка в ${country} буде коштувати ${credits} кредитів`);
+        break;
+    case 'індія':
+        credits = 80;
+        alert(`Доставка в ${country} буде коштувати ${credits} кредитів`);
+        break;
+    case 'ямайка':
+        credits = 120;
+        alert(`Доставка в ${country} буде коштувати ${credits} кредитів`);
+        break;
+    
+    default:
+        alert('У вашій країні доставка недоступна');
+}
