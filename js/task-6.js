9870ce689e844c25ac8c09703d09edbc576045f1
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-02 09:43:28 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -13,27 +13,3 @@ do {
 } while (input !== null);
 
 alert(`Загальна сума чисел дорівнює ${total}`)
-
-
-
-
-
-
-
-///Цикл з рандомізацією (ЗАМІТКИ)
-// const minSalary = 500;
-// const maxSalary = 5000;
-// const employees = 17;
-// let totalSalary = 0;
-
-// for (let i = 1; i <= 10; i += 1){
-//     const salary = Math.round(
-//         Math.random() * (maxSalary - minSalary) + minSalary
-//     );
-
-//     console.log(`ЗП робітника номер ${i} - ${salary}`);
-
-//     totalSalary += salary;
-// }
-
-// console.log('totalSalary: ', totalSalary);
